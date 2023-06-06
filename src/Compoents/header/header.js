3c855a890a38723261db8ca82093199721858310
Rohitash Singh <rohitashmtr2002@gmail.com>
2023-06-06 13:39:33 +0530

    Update header.js

--- src/Compoents/header/header.js
+++ src/Compoents/header/header.js
@@ -3,8 +3,7 @@ import React from 'react'
 
 import logo from '../Image/stackshare.png';
 import img from '../Image/stackshare.png';
-
-
+import { Link } from 'react-router-dom';
 
 export default function header(props) {
     return (
@@ -13,7 +12,7 @@ export default function header(props) {
         <div className='container'>
             <div className=''>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-3">
-                    <a className="navbar-brand" href="/"><img src={logo} style={{width: "200px"}} /></a>
+                    <Link className="navbar-brand" to="/"><img src={logo} style={{width: "200px"}} /></Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -21,13 +20,13 @@ export default function header(props) {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item">
-                                <a className="nav-link text-light fs-5 pt-3" href="/feature">features </a>
+                                <Link className="nav-link text-light fs-5 pt-3" to="/feature">features </Link>
                             </li>
                             <li className="nav-item ">
-                                <a className="nav-link text-light fs-5 pt-3" href="/pricing">pricing </a>
+                                <Link className="nav-link text-light fs-5 pt-3" to="/pricing">pricing </Link>
                             </li>
                             <li className="nav-item ">
-                                <a className="nav-link text-light fs-5 pt-3" href="/contact">Contact Us </a>
+                                <Link className="nav-link text-light fs-5 pt-3" to="/contact">Contact Us </Link>
                             </li>
                         </ul>
                         {/* <button className='btn text-light bg-success px-4' ><a href="/login" className='text-decoration-none text-light ' >Log in</a></button> */}
