97a8e77b0b85e35aaa41b7caff367cf12982a5bc
Rohitash Singh <rohitashmtr2002@gmail.com>
2023-06-06 13:44:57 +0530

    Update header.js

--- src/Compoents/header.js
+++ src/Compoents/header.js
@@ -15,7 +15,7 @@ export default function header() {
             <div className=''>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-3">
                     <a className="navbar-brand" href="/">
-                        <img src={logo} style={{width: "200px"}} alt="Featured imag"/></a>
+                    <Link className="navbar-brand" to="/"><img src={logo} style={{width: "200px"}} /></Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -23,13 +23,13 @@ export default function header() {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item">
-                                <a className="nav-link text-light fs-5 pt-3" href="/feature">features </a>
+                                <Link className="nav-link text-light fs-5 pt-3" to="/feature">features </Link>
                             </li>
                             <li className="nav-item ">
-                                <a className="nav-link text-light fs-5 pt-3" href="/pricing">pricing </a>
+                                <Link className="nav-link text-light fs-5 pt-3" to="/pricing">pricing </Link>
                             </li>
                             <li className="nav-item ">
-                                <a className="nav-link text-light fs-5 pt-3" href="/contact">Contact Us </a>
+                                <Link className="nav-link text-light fs-5 pt-3" to="/contact">Contact Us </Link>
                             </li>
                         </ul>
                         {/* <button className='btn text-light bg-success px-4' ><a href="/login" className='text-decoration-none text-light ' >Log in</a></button> */}
